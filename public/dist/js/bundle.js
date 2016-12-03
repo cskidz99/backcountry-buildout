df1d4cf39d5d37eddf576c76af4f50b5c64201a2
Cameron Skidmore <cskidz99@gmail.com>
2016-12-02 19:12:46 -0700

    skeleton complete. basic functionality working.

--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -6,24 +6,99 @@ angular.module('bBuildout', ['ui.router']);
 angular.module("bBuildout").controller("mainCtrl", function ($scope, $state, $window, $sce) {
     var baseUrl = "http://www.backcountry.com";
 
-    // $scope.head1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.head1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
     $scope.changeLocationHead1 = function (head1SearchTerm) {
         $scope.head1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + head1SearchTerm);
     };
 
-    $scope.head2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    // var myIframe = document.getElementById('headBox1');
+    // myIframe.onload = function () {
+    //   myIframe.contentWindow.scrollTop(100);
+    // }
+
+    // $scope.head2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
     $scope.changeLocationHead2 = function (head2SearchTerm) {
         $scope.head2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + head2SearchTerm);
     };
 
-    $scope.torso1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    // $scope.hands1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationHands1 = function (hands1SearchTerm) {
+        $scope.hands1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + hands1SearchTerm);
+    };
+
+    // $scope.hands2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationHands2 = function (hands2SearchTerm) {
+        $scope.hands2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + hands2SearchTerm);
+    };
+
+    // $scope.hands3SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationHands3 = function (hands3SearchTerm) {
+        $scope.hands3SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + hands3SearchTerm);
+    };
+
+    // $scope.torso1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
     $scope.changeLocationTorso1 = function (torso1SearchTerm) {
         $scope.torso1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso1SearchTerm);
     };
 
-    $scope.hands1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
-    $scope.changeLocationHands1 = function (hands1SearchTerm) {
-        $scope.hands1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + hands1SearchTerm);
+    // $scope.torso2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationTorso2 = function (torso2SearchTerm) {
+        $scope.torso2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso2SearchTerm);
+    };
+
+    // $scope.torso3SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationTorso3 = function (torso3SearchTerm) {
+        $scope.torso3SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso3SearchTerm);
+    };
+
+    // $scope.torso4SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationTorso4 = function (torso4SearchTerm) {
+        $scope.torso4SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso4SearchTerm);
+    };
+
+    // $scope.torso5SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationTorso5 = function (torso5SearchTerm) {
+        $scope.torso5SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso5SearchTerm);
+    };
+
+    $scope.legs1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs1 = function (legs1SearchTerm) {
+        $scope.legs1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs1SearchTerm);
+    };
+
+    // $scope.legs2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs2 = function (legs2SearchTerm) {
+        $scope.legs2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs2SearchTerm);
+    };
+
+    // $scope.legs3SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs3 = function (legs3SearchTerm) {
+        $scope.legs3SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs3SearchTerm);
+    };
+
+    // $scope.legs4SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs4 = function (legs4SearchTerm) {
+        $scope.legs4SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs4SearchTerm);
+    };
+
+    // $scope.legs1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs1 = function (legs1SearchTerm) {
+        $scope.legs1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs1SearchTerm);
+    };
+
+    // $scope.feet1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationFeet1 = function (feet1SearchTerm) {
+        $scope.feet1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + feet1SearchTerm);
+    };
+
+    // $scope.feet2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationFeet2 = function (feet2SearchTerm) {
+        $scope.feet2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + feet2SearchTerm);
+    };
+
+    // $scope.shoes1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationShoes1 = function (shoes1SearchTerm) {
+        $scope.shoes1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + shoes1SearchTerm);
     };
 });
 //# sourceMappingURL=bundle.js.map
--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,24 +1,99 @@
 angular.module("bBuildout").controller("mainCtrl", function($scope, $state, $window, $sce){
     const baseUrl = "http://www.backcountry.com";
 
-    // $scope.head1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.head1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
     $scope.changeLocationHead1 = head1SearchTerm => {
         $scope.head1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + head1SearchTerm);
     }
 
-    $scope.head2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    // var myIframe = document.getElementById('headBox1');
+    // myIframe.onload = function () {
+    //   myIframe.contentWindow.scrollTop(100);
+    // }
+
+    // $scope.head2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
     $scope.changeLocationHead2 = head2SearchTerm => {
         $scope.head2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + head2SearchTerm);
     }
 
-    $scope.torso1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    // $scope.hands1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationHands1 = hands1SearchTerm => {
+        $scope.hands1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + hands1SearchTerm);
+    }
+
+    // $scope.hands2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationHands2 = hands2SearchTerm => {
+        $scope.hands2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + hands2SearchTerm);
+    }
+
+    // $scope.hands3SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationHands3 = hands3SearchTerm => {
+        $scope.hands3SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + hands3SearchTerm);
+    }
+
+    // $scope.torso1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
     $scope.changeLocationTorso1 = torso1SearchTerm => {
         $scope.torso1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso1SearchTerm);
     }
 
-    $scope.hands1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
-    $scope.changeLocationHands1 = hands1SearchTerm => {
-        $scope.hands1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + hands1SearchTerm);
+    // $scope.torso2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationTorso2 = torso2SearchTerm => {
+        $scope.torso2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso2SearchTerm);
+    }
+
+    // $scope.torso3SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationTorso3 = torso3SearchTerm => {
+        $scope.torso3SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso3SearchTerm);
+    }
+
+    // $scope.torso4SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationTorso4 = torso4SearchTerm => {
+        $scope.torso4SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso4SearchTerm);
+    }
+
+    // $scope.torso5SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationTorso5 = torso5SearchTerm => {
+        $scope.torso5SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + torso5SearchTerm);
+    }
+
+    $scope.legs1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs1 = legs1SearchTerm => {
+        $scope.legs1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs1SearchTerm);
+    }
+
+    // $scope.legs2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs2 = legs2SearchTerm => {
+        $scope.legs2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs2SearchTerm);
+    }
+
+    // $scope.legs3SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs3 = legs3SearchTerm => {
+        $scope.legs3SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs3SearchTerm);
+    }
+
+    // $scope.legs4SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs4 = legs4SearchTerm => {
+        $scope.legs4SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs4SearchTerm);
+    }
+
+    // $scope.legs1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationLegs1 = legs1SearchTerm => {
+        $scope.legs1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + legs1SearchTerm);
+    }
+
+    // $scope.feet1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationFeet1 = feet1SearchTerm => {
+        $scope.feet1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + feet1SearchTerm);
+    }
+
+    // $scope.feet2SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationFeet2 = feet2SearchTerm => {
+        $scope.feet2SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + feet2SearchTerm);
+    }
+
+    // $scope.shoes1SearchUrl = $sce.trustAsResourceUrl(baseUrl);
+    $scope.changeLocationShoes1 = shoes1SearchTerm => {
+        $scope.shoes1SearchUrl = $sce.trustAsResourceUrl(baseUrl + "/Store/catalog/search.jsp?s=u&q=" + shoes1SearchTerm);
     }
 
 
